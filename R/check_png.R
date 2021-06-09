f4307b144dcaadc6665af00689c1144f33e08a4e
Nick Kennedy <nick@nick-kennedy.com>
2021-06-10 00:33:07 +0100

    Appveyor and tests

--- R/check_png.R
+++ R/check_png.R
@@ -9,8 +9,8 @@
 check_png <- function(file_name) {
   grDevices::png(file_name)
   on.exit(grDevices::dev.off())
-  my_plot <- ggplot2::ggplot(data.frame(x = 1:10, ymin = 1:10, ymax = 10 + 1:10), aes(x)) +
-    ggplot2::geom_ribbon(aes(ymin = ymin, ymax = ymax), alpha = 0.5) +
+  my_plot <- ggplot2::ggplot(data.frame(x = 1:10, ymin = 1:10, ymax = 10 + 1:10), ggplot2::aes(x)) +
+    ggplot2::geom_ribbon(ggplot2::aes(ymin = ymin, ymax = ymax), alpha = 0.5) +
     ggplot2::coord_cartesian(c(1, 9))
   invisible(my_plot)
 }
